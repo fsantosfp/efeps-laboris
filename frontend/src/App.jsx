adc31bd26d273d8ccae7056483d293c61954679d
Fabio Santos <fsantos@Fabios-MacBook-Pro.local>
2025-10-03 23:32:50 -0300

    feat: add default layout for all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,18 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import ProtectedRoute from './components/ProtectedRoute';
 import JobDetailPage from "./pages/JobDetailPage";
 import CreateJobPage from "./pages/CreateJobPage";
+import Layout from "./components/Layout";
 
 function App(){
   return (
     <Routes>
-      <Route path="/" element={ <Navigate to="/dashboard" /> } />
       <Route path="/login" element={ <LoginPage/> } />
-      <Route path="/dashboard" element={<ProtectedRoute><DashboardPage/></ProtectedRoute>} />
-      <Route path="/jobs/new" element={<ProtectedRoute><CreateJobPage/></ProtectedRoute>}></Route>
-      <Route path="/jobs/:jobId" element={<ProtectedRoute><JobDetailPage/></ProtectedRoute>}></Route>
+      <Route path="/" element={<ProtectedRoute><Layout/></ProtectedRoute>}>
+        <Route index element={ <Navigate to="/dashboard" /> } />
+        <Route path="/dashboard" element={<ProtectedRoute><DashboardPage/></ProtectedRoute>} />
+        <Route path="/jobs/new" element={<ProtectedRoute><CreateJobPage/></ProtectedRoute>}></Route>
+        <Route path="/jobs/:jobId" element={<ProtectedRoute><JobDetailPage/></ProtectedRoute>}></Route>
+      </Route>
     </Routes>
   )
 }
---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { NavLink, Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+const Layout = () => {
+
+    const { logout } = useAuth();
+
+    const navLinkStyle = ({isActive}) => ({
+        margin: '0 10px',
+        color: isActive ? 'blue' : 'black',
+        textDecoration: 'none',
+        fontWeight: isActive ? 'bold' : 'normal'
+    });
+
+    return (
+        <div style={{padding:'20px'}}>
+            <header style={{display:'flex',justifyContent:'space-between',alignItems:'center',borderBottom: '1px solid #ccc',paddingBottom: '10px'}}>
+                <h1>Laboris</h1>
+                <nav>
+                    <NavLink to="/dashboard" style={navLinkStyle}>Dashboard</NavLink>
+                    <NavLink to="/reports" style={navLinkStyle}>Relatórios</NavLink>
+                </nav>
+                <button onClick={logout}>Sair</button>
+            </header>
+            <main style={{margin:'20px'}}>
+                <Outlet></Outlet>
+            </main>
+        </div>
+    )
+}
+
+export default Layout;
