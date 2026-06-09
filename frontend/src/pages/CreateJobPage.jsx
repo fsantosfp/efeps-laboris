d5f9cb83c242608375e7a14e83b17e15f472d90b
Fabio Santos <fsantos@Fabios-MacBook-Pro.local>
2026-06-08 23:15:12 -0400

    fix: resolve file name

--- frontend/src/pages/CreateJobPage.jsx
+++ frontend/src/pages/CreateJobPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import api from "../services/api";
-import PlacesAutocompleteInput from "../components/PlacesAutoCompleteInput";
+import PlacesAutocompleteInput from "../components/PlacesAutocompleteInput";
 import "./CreateJobPage.css";
 
 function CreateJobPage() {
